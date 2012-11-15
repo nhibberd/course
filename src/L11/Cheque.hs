764f7e4dd1f68d0dc63f79cae285c59bbebb7be5
Tony Morris <tmorris@tmorris.net>
2012-11-15 17:21:10 +1000

    cheque

--- src/L11/Cheque.hs
+++ src/L11/Cheque.hs
@@ -1,9 +1,11 @@
 module L11.Cheque where
 
 import Data.List
+import Data.Int
 import Data.Char
 import Data.Maybe
 import Control.Monad
+import Control.Monad.Instances
 
 data Numbers =
   Zero
@@ -18,29 +20,12 @@ data Numbers =
   | Nine
   deriving (Eq, Ord, Show, Enum)
 
-data Teens =
-  Eleven
-  | Twelve
-  | Thirteen
-  | Fourteen
-  | Fifthteen
-  | Sixteen
-  | Seventeen
-  | Eighteen
-  | Nineteen
-  deriving (Eq, Ord, Show, Enum)
 
-data Ty =
-  Twenty
-  | Thirty
-  | Forty
-  | Fifty
-  | Sixty
-  | Seventy
-  | Eighty
-  | Ninety
-  deriving (Eq, Ord, Show, Enum)
-
+data Digits =
+  D1 Numbers
+  | D2 Numbers Numbers
+  | D3 Numbers Numbers Numbers
+  deriving (Eq)
 
 fromChar ::
   Char
@@ -68,137 +53,141 @@ fromChar '9' =
 fromChar _ =
   Nothing
 
-fromTyNumber ::
-  Numbers
-  -> Ty
-fromTyNumber Two =
-  Twenty
-fromTyNumber Three =
-  Thirty
-fromTyNumber Four =
-  Forty
-fromTyNumber Five =
-  Fifty
-fromTyNumber Six =
-  Sixty
-fromTyNumber Seven =
-  Seventy
-fromTyNumber Eight =
-  Eighty
-fromTyNumber Nine =
-  Ninety
-
-toTeen ::
-  Numbers
-  -> Teens
-toTeen One =
-  Eleven
-toTeen Two =
-  Twelve
-toTeen Three =
-  Thirteen
-toTeen Four =
-  Fourteen
-toTeen Five =
-  Fifthteen
-toTeen Six =
-  Sixteen
-toTeen Seven =
-  Seventeen
-toTeen Eight =
-  Eighteen
-toTeen Nine =
-  Nineteen
-
-toDot ::
-  String
-  -> ([Numbers], String)
-toDot =
-  let toDot' x [] =
-        (x, [])
-      toDot' x (h:t) =
-        case fromChar h of
-          Just n -> toDot' (n:x) t
-          Nothing -> (if h == '.'
-                        then
-                          (,)
-                        else
-                           toDot') x t
-  in toDot' []
-
 cheque ::
   String
   -> String
 cheque s =
-  let (l,r) = toDot s
-  in case (l,r) of
-    l -> undefined--split3 l
-    r -> undefined
+  let (p,l,r) = toDot (dropWhile (`notElem` ('.':['1'..'9'])) s)
+      l' = case l of
+             [] -> "zero dollars"
+             [One] -> "one dollar"
+             _ -> ill l p ++ " dollars"
+      r' = case mapMaybe (fromChar) r of
+             [] -> "zero cents"
+             [Zero, One] -> "one cent"
+             (a:b:_) -> show (D2 a b) ++ " cents"
+             (a:_) -> show (D2 a Zero) ++ " cents"
+  in l' ++ " and " ++ r'
 
-data V12 a =
-  V0
-  | V1 a
-  | V2 a a
-  deriving (Eq, Show)
-
-split3 ::
-  [a]
-  -> (V12 a, [(a, a, a)])
-split3 [] = (V0, [])
-split3 [a] = (V1 a, [])
-split3 [a, b] = (V2 b a, [])
-split3 (a:b:c:rest) = let (z,zz) = split3 rest in
-                      (z, (c,b,a) : zz )
-
-trippleE ::
-  [(Numbers,Numbers,Numbers)]
+ill ::
+  [Numbers]
+  -> Bool
   -> String
-trippleE [] = ""
-trippleE [(a,b,c)] = show a ++ " hundred" ++ ( fromMaybe' ( fmap (" and " ++) (tyOut (b,c)) ))
+ill xx st =
+  let space "" =
+        ""
+      space x =
+        ' ':x
+      state a =
+        if not (null a) && st then "," else ""
+      todigits acc _ [] =
+        acc
+      todigits _ [] _ =
+        error "ha ha ha"
+      todigits acc (_:it) (Zero:Zero:Zero:t) =
+        todigits acc it t
+      todigits acc (ih:it) (a:b:c:t) =
+        todigits ((show (D3 c b a) ++ space ih ++ state acc):acc) it t
+      todigits acc (_:it) (Zero:Zero:t) =
+        todigits acc it t
+      todigits acc (ih:it) (a:b:t) =
+        todigits ((show (D2 b a) ++ space ih ++ state acc):acc) it t
+      todigits acc (_:it) (Zero:t) =
+        todigits acc it t
+      todigits acc (ih:_) (a:_) =
+        (show (D1 a) ++ space ih ++ state acc):acc
+  in unwords $ todigits [] illion xx
 
-v12E ::
-  V12 Char
-  -> String
-v12E V0 = ""
-v12E (V1 a) = fromMaybe' (fmap show (fromChar a))
-v12E (V2 a b) = fromMaybe' (fmap (\a' -> fromMaybe' (tyOut (a', fromMaybe Zero (fromChar b)))) (fromChar a))
 
-fromMaybe' ::
-  Maybe[Char]
-  -> [Char]
-fromMaybe' (Just a) = a
-fromMaybe' Nothing = ""
+toDot ::
+  String
+  -> (Bool, [Numbers], String)
+toDot s =
+  let toDot' ::
+        [Numbers]
+        -> Int
+        -> String
+        -> (Int, [Numbers], String)
+      toDot' x i [] =
+        (i, x, [])
+      toDot' x i (h:t) =
+        case fromChar h of
+          Just n -> toDot' (n:x) (i+1) t
+          Nothing -> (if h == '.'
+                        then
+                          (i, x, t)
+                        else
+                           toDot' x i t)
+      (i', m, s') = toDot' [] 0 s
+  in (i' >= 5, m, s')
 
 
-tyOut ::
-  (Numbers, Numbers)
-  -> Maybe String
-tyOut (Zero,Zero) = Nothing
-tyOut (Zero,b) = Just (show b)
-tyOut (a,Zero) = Just (show . fromTyNumber $ a)
-tyOut (One,b) = Just (show (toTeen b))
-tyOut (One,Zero) = Just "Ten"
-tyOut (a,b) = let z = fromTyNumber a
-              in  Just (show z ++ " " ++ show b)
+instance Show Digits where
+  show d =
+    let showd x = case show x of
+                  (h:t) -> (toLower h):t
+                  [] -> ""
+        x .++. y = x ++ if y == Zero then [] else '-' : showd y
+    in case d of
+        D1 a -> showd a
+        D2 Zero b -> showd b
+        D2 One b -> case b of
+                      Zero -> "ten"
+                      One -> "eleven"
+                      Two -> "twelve"
+                      Three -> "thirteen"
+                      Four -> "fourteen"
+                      Five -> "fifteen"
+                      Six -> "sixteen"
+                      Seven -> "seventeen"
+                      Eight -> "eighteen"
+                      Nine -> "nineteen"
+        D2 Two b -> "twenty" .++. b
+        D2 Three b -> "thirty" .++. b
+        D2 Four b -> "forty" .++. b
+        D2 Five b -> "fifty" .++. b
+        D2 Six b -> "sixty" .++. b
+        D2 Seven b -> "seventy" .++. b
+        D2 Eight b -> "eighty" .++. b
+        D2 Nine b -> "ninety" .++. b
+        D3 Zero Zero Zero -> ""
+        D3 Zero b c -> show (D2 b c)
+        D3 a Zero Zero -> showd a ++ " hundred"
+        D3 a b c -> showd a ++ " hundred and " ++ show (D2 b c)
 
-r ::
-  [(String, String)]
-r = [("0", "zero dollars and zero cents"),
-     ("1", "one dollar and zero cents"),
-     ("1.", "one dollar and zero cents"),
-     ("0.", "zero dollars and zero cents"),
-     ("0.0", "zero dollars and zero cents"),
-     ("1.0", "one dollar and zero cents"),
-     ("a1a", "one dollar and zero cents"),
-     ("a1a.a0.7b", "one dollar and seven cents"),
-     ("100", "one hundred dollars and zero cents"),
-     ("100.45", "one hundred dollars and forty-five cents"),
-     ("100.07", "one hundred dollars and seven cents"),
-     ("9abc9def9ghi.jkl9mno", "nine hundred and ninety-nine dollars and ninety cents"),
-     ("12345.67", "twelve thousand three hundred and forty-five dollars and sixty-seven cents"),
-     ("456789123456789012345678901234567890123456789012345678901234567890.12", "four hundred and fifty-six vigintillion seven hundred and eighty-nine novemdecillion one hundred and twenty-three octodecillion four hundred and fifty-six septendecillion seven hundred and eighty-nine sexdecillion twelve quindecillion three hundred and forty-five quattuordecillion six hundred and seventy-eight tredecillion nine hundred and one duodecillion two hundred and thirty-four undecillion five hundred and sixty-seven decillion eight hundred and ninety nonillion one hundred and twenty-three octillion four hundred and fifty-six septillion seven hundred and eighty-nine sextillion twelve quintillion three hundred and forty-five quadrillion six hundred and seventy-eight trillion nine hundred and one billion two hundred and thirty-four million five hundred and sixty-seven thousand eight hundred and ninety dollars and twelve cents")
+
+
+test ::
+  IO ()
+test =
+  mapM_ (\(i, o) -> let r = cheque i
+                    in when (o /= r) (mapM_ putStrLn [i, '\t':o, '\t':r]))
+    [
+      ("0", "zero dollars and zero cents")
+    , ("1", "one dollar and zero cents")
+    , ("0.1", "zero dollars and ten cents")
+    , ("1.", "one dollar and zero cents")
+    , ("0.", "zero dollars and zero cents")
+    , ("0.0", "zero dollars and zero cents")
+    , (".34", "zero dollars and thirty-four cents")
+    , ("0.3456789", "zero dollars and thirty-four cents")
+    , ("1.0", "one dollar and zero cents")
+    , ("1.01", "one dollar and one cent")
+    , ("a1a", "one dollar and zero cents")
+    , ("a1a.a0.7b", "one dollar and seven cents")
+    , ("100", "one hundred dollars and zero cents")
+    , ("100.0", "one hundred dollars and zero cents")
+    , ("100.00", "one hundred dollars and zero cents")
+    , ("100.00000", "one hundred dollars and zero cents")
+    , ("1000456.13", "one million, four hundred and fifty-six dollars and thirteen cents")
+    , ("1001456.13", "one million, one thousand, four hundred and fifty-six dollars and thirteen cents")
+    , ("16000000456.13", "sixteen billion, four hundred and fifty-six dollars and thirteen cents")
+    , ("100.45", "one hundred dollars and forty-five cents")
+    , ("100.07", "one hundred dollars and seven cents")
+    , ("9abc9def9ghi.jkl9mno", "nine hundred and ninety-nine dollars and ninety cents")
+    , ("12345.67", "twelve thousand, three hundred and forty-five dollars and sixty-seven cents")
+    , ("456789123456789012345678901234567890123456789012345678901234567890.12", "four hundred and fifty-six vigintillion, seven hundred and eighty-nine novemdecillion, one hundred and twenty-three octodecillion, four hundred and fifty-six septendecillion, seven hundred and eighty-nine sexdecillion, twelve quindecillion, three hundred and forty-five quattuordecillion, six hundred and seventy-eight tredecillion, nine hundred and one duodecillion, two hundred and thirty-four undecillion, five hundred and sixty-seven decillion, eight hundred and ninety nonillion, one hundred and twenty-three octillion, four hundred and fifty-six septillion, seven hundred and eighty-nine sextillion, twelve quintillion, three hundred and forty-five quadrillion, six hundred and seventy-eight trillion, nine hundred and one billion, two hundred and thirty-four million, five hundred and sixty-seven thousand, eight hundred and ninety dollars and twelve cents")
     ]
-
 
 illion ::
   [String]
@@ -322,7 +311,8 @@ illion =
         , "nonagintanongentillion"
         ]
   in [
-       "thousand"
+       ""
+     , "thousand"
      , "million"
      , "billion"
      , "trillion"
